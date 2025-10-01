c3c7846be1c513b9c959198cd71650e22d716dcf
imdhairyathakkar-spec <imdhairyathakkar@gmail.com>
2025-10-01 17:50:26 +0530

    Create EXP10-b.cpp

---
+++ EXP10-b.cpp
@@ -0,0 +1,27 @@
+//Dhairya Thakkar
+//2470123037
+//A2
+//call by reference
+#include <iostream>
+using namespace std;
+void swap(int *x, int *y){
+    int aa;
+    aa=*x;
+    *x=*y;
+    *y=aa;
+cout << "swapped value of a "<< *x<<endl;
+cout <<"swapped value of b "<<  *y<< endl;
+}
+int main(){
+ int a=3,b=5;
+ swap(&a,&b);
+ cout << a<<endl;
+ cout<< b;
+}
+/* OUTPUT:
+swapped value of a 5
+swapped value of b 3
+5
+3
+
+*/
